10e782f1c5df834405c106fd5b7d927b7735d69f
Kaina Cockett <kainacockett@gmail.com>
2023-08-22 12:59:13 -0600

    Lesson complete

--- spec/features/user_can_search_by_senator_last_name_spec.rb
+++ spec/features/user_can_search_by_senator_last_name_spec.rb
@@ -3,25 +3,29 @@ require 'rails_helper'
 RSpec.describe 'Govt Search' do
   describe 'happy path' do
     it 'allows user to search for govt members' do
-      visit root_path
+      VCR.use_cassette("propublica_senators") do
+        visit root_path
 
-      fill_in :search, with: 'Sanders'
-      click_button 'Search'
+        fill_in :search, with: 'Sanders'
+        click_button 'Search'
 
-      expect(page.status_code).to eq 200
-      expect(page).to have_content("Senator Bernard Sanders was found!")
-      expect(page).to have_content("SenSanders")
+        expect(page.status_code).to eq 200
+        expect(page).to have_content("Senator Bernard Sanders was found!")
+        expect(page).to have_content("SenSanders")
+      end
     end
 
     it 'allows user to search for another govt member' do
-      visit root_path
+      VCR.use_cassette("propublica_senators") do
+        visit root_path
 
-      fill_in :search, with: 'Booker'
-      click_button 'Search'
+        fill_in :search, with: 'Booker'
+        click_button 'Search'
 
-      expect(page.status_code).to eq 200
-      expect(page).to have_content("Senator Cory Booker was found!")
-      expect(page).to have_content("SenBooker")
+        expect(page.status_code).to eq 200
+        expect(page).to have_content("Senator Cory Booker was found!")
+        expect(page).to have_content("SenBooker")
+      end
     end
   end
 end
